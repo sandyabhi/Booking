f076ff50dcaa9d717651ad12f0f81b4155e97c41
Sandeep Kumar Patel <abhisandy47@gmail.com>
2023-10-03 21:19:03 +0530

    listings, listing view, reservation

---
+++ app/components/listings/ListingHead.tsx
@@ -0,0 +1,50 @@
+"use client";
+
+import Image from "next/image";
+import useCountries from "@/app/hooks/useCountries";
+import { SafeUser } from "@/app/types";
+import Heading from "../Heading";
+import HeartButton from "../HeartButton";
+
+interface ListingHeadProps {
+  title: string;
+  locationValue: string;
+  imageSrc: string;
+  id: string;
+  currentUser?: SafeUser | null;
+}
+
+const ListingHead = ({
+  title,
+  locationValue,
+  imageSrc,
+  id,
+  currentUser,
+}: ListingHeadProps) => {
+  const { getByValue } = useCountries();
+
+  const location = getByValue(locationValue);
+
+  return (
+    <>
+      <Heading
+        title={title}
+        subtitle={`${location?.region}, ${location?.label}`}
+        center
+      />
+      <div className="w-full overflow-hidden h-[60vh] rounded-xl relative">
+        <Image
+          src={imageSrc}
+          fill
+          className="object-cover w-full"
+          alt="Image"
+        />
+        <div className="absolute top-5 right-5">
+          <HeartButton listingId={id} currentUser={currentUser} />
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default ListingHead;
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,30 @@
-export default function Home() {
-  return <main></main>;
+import getCurrentUser from "./actions/getCurrentUser";
+import getListings from "./actions/getListings";
+import Container from "./components/Container";
+import EmptyState from "./components/EmptyState";
+import ListingCard from "./components/listings/ListingCard";
+import { SafeListing } from "./types";
+
+export default async function Home() {
+  const listings = await getListings();
+  const currentUser = await getCurrentUser();
+
+  // console.log(listings);
+
+  if (listings.length === 0) {
+    return <EmptyState showReset />;
+  }
+  return (
+    <Container>
+      <div className="pt-28 grid grid-cols-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8">
+        {listings.map((listing: SafeListing) => (
+          <ListingCard
+            key={listing.id}
+            currentUser={currentUser}
+            data={listing}
+          />
+        ))}
+      </div>
+    </Container>
+  );
 }
